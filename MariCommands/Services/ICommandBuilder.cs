14e9a7313197ee479d03f527f3fc3a6b52a16604
renanrcp <renancamerapinheiro@gmail.com>
2020-06-10 19:49:53 -0300

    ICommandBuilder

---
+++ MariCommands/Services/ICommandBuilder.cs
@@ -0,0 +1,10 @@
+namespace MariCommands.Services
+{
+    /// <summary>
+    /// A service that can build commands.
+    /// </summary>
+    public interface ICommandBuilder
+    {
+        //TODO: BuildMethods
+    }
+}
