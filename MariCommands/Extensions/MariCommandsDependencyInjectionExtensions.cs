f7b66496229242597c5e524f21fe28ef741f9c99
renanrcp <renancamerapinheiro@gmail.com>
2020-06-10 01:17:40 -0300

    provides a default IServiceProvider

--- MariCommands/Extensions/MariCommandsDependencyInjectionExtensions.cs
+++ MariCommands/Extensions/MariCommandsDependencyInjectionExtensions.cs
@@ -1,5 +1,7 @@
 using System;
+using MariGlobals.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MariCommands
 {
@@ -11,7 +13,31 @@ namespace MariCommands
         internal static T GetOrDefault<T, TDefault>(this IServiceProvider provider)
             where TDefault : T, new()
         {
+            if (provider.HasNoContent())
+                return new TDefault();
+
             return provider.GetService<T>() ?? new TDefault();
+        }
+
+        internal static T GetOrDefault<T, TDefault>(this IServiceProvider provider, TDefault defaultValue)
+            where TDefault : T, new()
+        {
+            if (provider.HasNoContent())
+                return defaultValue;
+
+            return provider.GetService<T>() ?? defaultValue;
+        }
+
+        internal static IServiceProvider CreateDefaultServiceProvider()
+        {
+            var collection = new ServiceCollection();
+
+            collection.AddSingleton<ILoggerFactory, LoggerFactory>();
+            //TODO: collection.AddSingleton<IModuleBuilder, ModuleBuilder>();
+            //TODO: collection.AddSingleton<IModuleSaver, ModuleSaver>();
+            //TODO: collection.AddSingleton<ICommandExecutor, CommandExecutor();
+
+            return collection.BuildServiceProvider();
         }
     }
 }
