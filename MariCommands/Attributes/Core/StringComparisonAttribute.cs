028cf96f3b9c3d893160df019c81d6011aa7c38e
renanrcp <renancamerapinheiro@gmail.com>
2020-06-13 00:11:05 -0300

    StringComparisonAttribute

---
+++ MariCommands/Attributes/Core/StringComparisonAttribute.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MariCommands.Attributes.Core
+{
+    /// <summary>
+    /// Defines the <see cref="StringComparison" /> for search commands in this module or command.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class StringComparisonAttribute : Attribute
+    {
+        /// <summary>
+        /// The <see cref="StringComparison" /> for search commands in this module or command.
+        /// </summary>
+        public StringComparison Value { get; }
+
+
+        /// <summary>
+        /// Creates a new instance off <see cref="StringComparisonAttribute" />.
+        /// </summary>
+        /// <param name="comparisonType">The <see cref="StringComparison" /> for search commands in this module or command.</param>
+        public StringComparisonAttribute(StringComparison comparisonType)
+        {
+            Value = comparisonType;
+        }
+    }
+}
