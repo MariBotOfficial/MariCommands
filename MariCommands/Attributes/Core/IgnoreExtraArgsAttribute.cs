2240fe52c1806c6e337a1136e39f373b00bf1406
renanrcp <renancamerapinheiro@gmail.com>
2020-06-12 23:48:37 -0300

    IgnoreExtraArgsAttribute

---
+++ MariCommands/Attributes/Core/IgnoreExtraArgsAttribute.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace MariCommands.Attributes.Core
+{
+    /// <summary>
+    /// Defines if this module or command will ignore extra args.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class IgnoreExtraArgsAttribute : Attribute
+    {
+        /// <summary>
+        /// If this module or command will ignore extra args.
+        /// </summary>
+        public bool Value { get; }
+
+        /// <summary>
+        /// Creates a new instance off <see cref="IgnoreExtraArgsAttribute" />
+        /// with value: <see langword="true" />.
+        /// </summary>
+        public IgnoreExtraArgsAttribute() : this(true)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instance off <see cref="IgnoreExtraArgsAttribute" />.
+        /// </summary>
+        /// <param name="ignoreExtraArgs">If this module or command will ignore extra args.</param>
+        public IgnoreExtraArgsAttribute(bool ignoreExtraArgs)
+        {
+            Value = ignoreExtraArgs;
+        }
+    }
+}
