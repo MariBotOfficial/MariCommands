ad3e7b0fdab9f3d2cc9d919864e8d256e310fbfa
renanrcp <renancamerapinheiro@gmail.com>
2020-06-12 23:53:58 -0300

    RunModeAttribute

---
+++ MariCommands/Attributes/Core/RunModeAttribute.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace MariCommands
+{
+    /// <summary>
+    /// Defines the <see cref="RunMode" /> for this module or command.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RunModeAttribute : Attribute
+    {
+        /// <summary>
+        /// The <see cref="RunMode" /> for this module or command.
+        /// </summary>
+        public RunMode Value { get; }
+
+        /// <summary>
+        /// Creates a new instance off <see cref="RunModeAttribute" />.
+        /// </summary>
+        /// <param name="runMode">The <see cref="RunMode" /> for this module or command.</param>
+        public RunModeAttribute(RunMode runMode)
+        {
+            Value = runMode;
+        }
+    }
+}
