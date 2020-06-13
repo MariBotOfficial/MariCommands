27c1e926734ddda95f46f1f8ce4ad1674122fbfe
renanrcp <renancamerapinheiro@gmail.com>
2020-06-12 23:18:05 -0300

    namespace fix for GroupAttribute

--- MariCommands/Attributes/Modules/GroupAttribute.cs
+++ MariCommands/Attributes/Modules/GroupAttribute.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 
-namespace MariCommands.Attributes.Modules
+namespace MariCommands
 {
     /// <summary>
     /// Mark a class as group of the parent module.
