5c20cc19b8af3d46b318381119ea1385ed8b3a81
renanrcp <renancamerapinheiro@gmail.com>
2020-06-13 16:25:18 -0300

    ParseAsync signature in ITypeParser

--- MariCommands/TypeParsers/ITypeParser.cs
+++ MariCommands/TypeParsers/ITypeParser.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 namespace MariCommands
 {
     /// <summary>
@@ -6,6 +8,14 @@ namespace MariCommands
     /// <typeparam name="T"></typeparam>
     public interface ITypeParser<T>
     {
-        // TODO: ParseAsync
+        /// <summary>
+        /// Asynchronously parse a raw value to the specified type.
+        /// </summary>
+        /// <param name="value">The raw argument value.</param>
+        /// <param name="parameter">The specified param to be parsed.</param>
+        /// <param name="context">The current command execution context.</param>
+        /// <returns>A <see cref="Task" /> representing an asynchronous operation with an
+        /// <see cref="ITypeParserResult{T}" />.</returns>
+        Task<ITypeParserResult<T>> ParseAsync(string value, IParameter parameter, ICommandContext context);
     }
 }
