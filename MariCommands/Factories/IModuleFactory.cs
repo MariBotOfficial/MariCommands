82f8e13a9f4e940bd26ae4a56bf73922fe606cc7
renanrcp <renancamerapinheiro@gmail.com>
2020-06-13 18:11:54 -0300

    Fix in signatures and docs

--- MariCommands/Factories/IModuleFactory.cs
+++ MariCommands/Factories/IModuleFactory.cs
@@ -15,5 +15,13 @@ namespace MariCommands.Factories
         /// <returns>A <see cref="Task" /> representing an asynchronous operation
         /// with a builded module.</returns>
         Task<IModuleBuilder> BuildModuleAsync(Type type);
+
+        /// <summary>
+        /// Asynchronously verify if the type is a module.
+        /// </summary>
+        /// <param name="type">The module type.</param>
+        /// <returns>A <see cref="Task" /> representing an asynchronous operation
+        /// with a boolean.</returns>
+        Task<bool> IsModuleAsync(Type type);
     }
 }
