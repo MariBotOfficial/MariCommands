5a3fe39766f38c0c53e0df6276e7ce2fa98b683f
renanrcp <renancamerapinheiro@gmail.com>
2020-06-13 23:38:32 -0300

    fix and code review from CommandFactory

--- MariCommands/Factories/CommandFactory.cs
+++ MariCommands/Factories/CommandFactory.cs
@@ -6,33 +6,36 @@ using MariGlobals.Extensions;
 
 namespace MariCommands
 {
+    /// <inheritdoc />
     public class CommandFactory : ICommandFactory
     {
+        private readonly IServiceProvider _provider;
+        private readonly ICommandServiceOptions _config;
 
         /// <summary>
-        ///     Create a news instace of <see  cref="CommandFactory"/>
+        /// Create a new instace of <see  cref="CommandFactory"/>.
         /// </summary>
-        /// <param name="provider">An <see cref="IServiceProvider" />.</param>
-
-        private readonly IServiceProvider _provider;
-
-
+        /// <param name="provider">A dependency container.</param>
         public CommandFactory(IServiceProvider provider)
         {
-            _provider = provider;
+            _provider = provider ?? ServiceUtils.CreateDefaultServiceProvider();
+            _config = _provider.GetOrDefault<ICommandServiceOptions, CommandServiceOptions>();
         }
 
 
-        /// <inheritdoc>
+        /// <inheritdoc />
         public async Task<ICommandBuilder> BuildCommandAsync(Type type, MethodInfo methodInfo)
         {
-            if (await IsCommandAsync(type, methodInfo))
-                throw new Exception($"man this command({methodInfo}) is not valid, please pay attention");
-            
+            type.NotNull(nameof(type));
+            methodInfo.NotNull(nameof(methodInfo));
+
+            if (!await IsCommandAsync(type, methodInfo))
+                throw new ArgumentException(nameof(type), $"{methodInfo.Name} is not a valid command.");
+
             return null;
         }
 
-        /// <inheritdoc>
+        /// <inheritdoc />
         public Task<bool> IsCommandAsync(Type type, MethodInfo methodInfo)
         {
             var isValid = type.HasContent() &&
--- MariCommands/Models/Commands/CommandBuilder.cs
+++ MariCommands/Models/Commands/CommandBuilder.cs
@@ -4,36 +4,52 @@ using System.Reflection;
 
 namespace MariCommands
 {
+    /// <inheritdoc />
     public class CommandBuilder : ICommandBuilder
     {
+        /// <inheritdoc />
         public string Name { get; private set; }
 
+        /// <inheritdoc />
         public string Description { get; private set; }
 
+        /// <inheritdoc />
         public string Remarks { get; private set; }
 
+        /// <inheritdoc />
         public int Priority { get; private set; }
 
+        /// <inheritdoc />
         public RunMode RunMode { get; private set; }
 
+        /// <inheritdoc />
         public bool IgnoreExtraArgs { get; private set; }
 
+        /// <inheritdoc />
         public Type ArgumentParserType { get; private set; }
 
+        /// <inheritdoc />
         public string Separator { get; private set; }
 
+        /// <inheritdoc />
         public StringComparison? Comparison { get; private set; }
 
+        /// <inheritdoc />
         public IReadOnlyCollection<string> Aliases { get; private set; }
 
+        /// <inheritdoc />
         public IReadOnlyCollection<Attribute> Attributes { get; private set; }
 
+        /// <inheritdoc />
         public IReadOnlyCollection<PreconditionAttribute> Preconditions { get; private set; }
 
+        /// <inheritdoc />
         public IReadOnlyCollection<IParameter> Parameters { get; private set; }
 
+        /// <inheritdoc />
         public bool IsEnabled { get; private set; }
 
+        /// <inheritdoc />    
         public MethodInfo Method { get; private set; }
     }
 }
