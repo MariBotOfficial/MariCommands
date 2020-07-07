0391d2567bd8e0fe1b098edade8be44a1e93dacf
renanrcp <renancamerapinheiro@gmail.com>
2020-07-06 23:13:47 -0300

    CommandDelegate docs

--- MariCommands/Models/Core/CommandDelegate.cs
+++ MariCommands/Models/Core/CommandDelegate.cs
@@ -2,5 +2,10 @@ using System.Threading.Tasks;
 
 namespace MariCommands
 {
+    /// <summary>
+    /// A delegate that can proccess a command request.
+    /// </summary>
+    /// <param name="context">The current command context.</param>
+    /// <returns>A <see cref="Task" /> representing an asynchronous operation</returns>
     public delegate Task CommandDelegate(CommandContext context);
 }
