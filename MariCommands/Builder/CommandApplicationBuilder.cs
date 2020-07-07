01adf44faeccf51c46317c33fb6247c3fd098625
renanrcp <renancamerapinheiro@gmail.com>
2020-07-06 23:37:02 -0300

    CommandApplicationBuilder

---
+++ MariCommands/Builder/CommandApplicationBuilder.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MariGlobals.Extensions;
+
+namespace MariCommands.Builder
+{
+    /// <inheritdoc />
+    public class CommandApplicationBuilder : ICommandApplicationBuilder
+    {
+        private readonly List<Func<CommandDelegate, CommandDelegate>> _components;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CommandApplicationBuilder" />.
+        /// </summary>
+        /// <param name="properties">The properties of this builder.</param>
+        /// <param name="applicationServices">The service container for this application.</param>
+        public CommandApplicationBuilder(IDictionary<string, object> properties, IServiceProvider applicationServices)
+        {
+            Properties = properties;
+            ApplicationServices = applicationServices;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CommandApplicationBuilder" />.
+        /// </summary>
+        /// <param name="applicationServices">The service container for this application.</param>
+        public CommandApplicationBuilder(IServiceProvider applicationServices)
+            : this(new Dictionary<string, object>(), applicationServices)
+        {
+        }
+
+        /// <inheritdoc />
+        public IDictionary<string, object> Properties { get; }
+
+        /// <inheritdoc />
+        public IServiceProvider ApplicationServices { get; }
+
+        /// <inheritdoc />
+        public ICommandApplicationBuilder Use(Func<CommandDelegate, CommandDelegate> component)
+        {
+            _components.Add(component);
+            return this;
+        }
+
+        /// <inheritdoc />
+        public CommandDelegate Build()
+        {
+            CommandDelegate app = context =>
+            {
+                context.NotNull(nameof(context));
+
+                var command = context.Command;
+
+                if (command.HasNoContent())
+                {
+                    // TODO: Put the parser middleware here.
+                    var message =
+                        $"The request reached the end of the pipeline without a command defined";
+                }
+
+                if (context.Result.HasNoContent())
+                {
+                    // TODO: Put the executor middleware here.
+                    var message =
+                        $"The request reached the end of the pipeline without executing the command: '{command.Name}'.";
+                }
+
+                return Task.CompletedTask;
+            };
+
+            _components.Reverse();
+
+            foreach (var component in _components)
+                app = component(app);
+
+            return app;
+        }
+    }
+}
--- MariCommands/Builder/ICommandApplicationBuilder.cs
+++ MariCommands/Builder/ICommandApplicationBuilder.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace MariCommands.Builder
 {
     /// <summary>
@@ -5,5 +8,27 @@ namespace MariCommands.Builder
     /// </summary>
     public interface ICommandApplicationBuilder
     {
+        /// <summary>
+        /// Properties to be used across middlewares.
+        /// </summary>
+        IDictionary<string, object> Properties { get; }
+
+        /// <summary>
+        /// The general service container for this application.
+        /// </summary>
+        IServiceProvider ApplicationServices { get; }
+
+        /// <summary>
+        /// Add a middleware to the command execution pipeline.
+        /// </summary>
+        /// <param name="component">The middleware function to execute the current command request.</param>
+        /// <returns>The current command application builder.</returns>
+        ICommandApplicationBuilder Use(Func<CommandDelegate, CommandDelegate> component);
+
+        /// <summary>
+        /// Build the command execution pipeline.
+        /// </summary>
+        /// <returns></returns>
+        CommandDelegate Build();
     }
 }
