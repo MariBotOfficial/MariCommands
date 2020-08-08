ed322042d16eaa7f21db4714373c6da8be24d794
renanrcp <renancamerapinheiro@gmail.com>
2020-08-08 11:55:18 -0300

    ops

--- MariCommands/Models/Core/CommandServiceOptions.cs
+++ MariCommands/Models/Core/CommandServiceOptions.cs
@@ -28,5 +28,8 @@ namespace MariCommands
 
         /// <inheritdoc />
         public bool AutoAddRunningAssembly { get; set; } = false;
+
+        /// <inheritdoc />
+        public bool ContinueMultiMatchAfterParser { get; set; } = false;
     }
 }
