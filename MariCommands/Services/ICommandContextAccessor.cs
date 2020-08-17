da23fdd81746a14e21885402aa8fb3218c7423e4
renanrcp <renancamerapinheiro@gmail.com>
2020-08-17 14:29:16 -0300

    changed ICommandContextAccessor.Context to ICommandContextAccessor.CommandContext

--- MariCommands/Services/ICommandContextAccessor.cs
+++ MariCommands/Services/ICommandContextAccessor.cs
@@ -1,15 +1,15 @@
 namespace MariCommands
 {
     /// <summary>
-    /// An interface taht can access the current <see cref="CommandContext" /> of
+    /// An interface taht can access the current <see cref="MariCommands.CommandContext" /> of
     /// the current scoped execution context.
     /// </summary>
     public interface ICommandContextAccessor
     {
         /// <summary>
-        /// Gets or sets the current <see cref="CommandContext" /> of the current scoped 
+        /// Gets or sets the current <see cref="MariCommands.CommandContext" /> of the current scoped 
         /// execution context.
         /// </summary>
-        CommandContext Context { get; set; }
+        CommandContext CommandContext { get; set; }
     }
 }
--- MariCommands/Services/ScopedCommandContextAccessor.cs
+++ MariCommands/Services/ScopedCommandContextAccessor.cs
@@ -3,6 +3,6 @@ namespace MariCommands
     // This implementation is just a scoped dependency, doesn't anything too complex.
     internal sealed class ScopedCommandContextAccessor : ICommandContextAccessor
     {
-        public CommandContext Context { get; set; }
+        public CommandContext CommandContext { get; set; }
     }
 }
